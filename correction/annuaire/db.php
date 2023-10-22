75ffbdd7c952d4e01747385bf3715c6f02ad8336
theovbl <tvirbel@gmail.com>
2023-10-22 17:11:58 +0200

    Commit all

--- correction/annuaire/db.php
+++ correction/annuaire/db.php
@@ -1,3 +1,9 @@
 <?php
-$bdd = new PDO ('mysql:host=localhost;dbname=cours;charset=utf8','Theo','Azerty123'); 
+$bdd = new PDO ('mysql:host=localhost;dbname=cours;charset=utf8','THEO','Azerty123'); 
 // Connexion à la base de donnée mysql avec ses identifiants
+$("#effacertout").click(function(){
+    $("#listeB option").appendTo("#listeA");
+});
+$("#ajouterTout").click(function(){
+    $("#listeA option").appendTo("#listeB");
+});
