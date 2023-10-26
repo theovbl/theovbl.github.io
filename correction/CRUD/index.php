a1b951a7a9952d72eb459c9f1bcf38251c174f15
theovbl <tvirbel@gmail.com>
2023-10-26 16:26:53 +0200

    Commit all

--- correction/CRUD/index.php
+++ correction/CRUD/index.php
@@ -1,16 +1,23 @@
 <?php
-    require_once('db.php')
+    require_once('./Controllers/read_ctrl.php');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        th, td {
+            border: 1px solid black;
+            padding: 25px;
+        }
+    </style>
 </head>
 <body>
-    <!-- CRUD: CREATE, READ(SELECT), UPDATE, DELETE -->
-    <!-- Vous allez avoir plusieurs fichier 
+    <!-- CRUD: CREATE(INSERT INTO), READ(SELECT), UPDATE, DELETE
+
+    
+    Vous allez avoir plusieurs fichier 
     Dans un dossier 'Views' vous avez: create.php, read.php, update.php
     Dans un dossier 'Controllers' vous avez: create_ctrl.php, read_ctrl.php, update_ctrl.php, delete_ctrl.php 
 
@@ -26,62 +33,46 @@
     L'index.php affichera un tableau de user, chaque ligne de ce tableau affichera les informations 
     (id, pseudo, mot de passe hashé, description) de cet user et permettra aussi la suppression, 
     la modification et l'affichage d'un user via un bouton ou un lien
-    L'index.php affichera  aussu un bouton permettant la création d'un user
+    L'index.php affichera aussi un bouton permettant la création d'un user
     create.php affichera le formulaire de création d'user
     update.php affichera le formulaire prérempli d'user afin de la modifier
     read.php afficher une liste à puce des informations de l'user
 
-    
 -->
-<table>
+    <form action="Views/create.php" method="post">        
+        <button type="submit">Ajouter utilisateur</button>
+        <a href="Views/login.php">Connexion</a>
+        <br><br><br><br>
+
+        <table>
             <tr>
-                <th>Identification :</th>
-                <th>Pseudo:</th>
-                <th>Mot-de-passe :</th>
+                <th>ID :</th>
+                <th>Pseudo :</th>
+                <th>Mot de passe :</th>
                 <th>Description :</th>
-             
+                <th>Actions :</th>
             </tr>
             <?php
-                $select = $bdd->prepare('SELECT * FROM user');
-                $select->execute();
-                $select = $select->fetchAll();
-                if (!empty($select)) {
-                    # Le foreach prend tout ce qui ce trouve dans $select et le range dans les variable $index qui sera l'index
-                    # et valeur qui est son les valeur de l'index
-                    foreach ($select as $valeur) {
-                        $id = $valeur['id'];
-                        $pseudo = $valeur['pseudo'];
-                        $motdepasse = $valeur['mot_de_passe'];
-                        $description = $valeur['description'];
-                       
-
-                        echo "<tr> 
-                        <td> $id </td>
-                        <td> $pseudo </td>
-                        <td> $motdepasse </td>
-                        <td> $description </td>
-                        <td>
-                            <form method='post' action=''>
-                                <input type='hidden' name='remove' value='$id'>
-                                <button type='submit'>Supprimer</button>
-                                <input type='hidden' name='remove' value='$id'>
-                                <button type='submit'>Modifier</button>
-                            </form>
-                        </td>
-                    </tr>";
-                }
-        
-                if (isset($_POST['remove'])) {
-                    $removeID = $_POST['remove'];
-                    $delete = $bdd->prepare('DELETE FROM user WHERE id = ?');
-                    $delete->execute([$removeID]);
-        
+            //Ligne prend la valeur de la variable $Tableau
+                foreach ($Tableau as $ligne) {
+                    echo '<tr>';
+            // $column est un tableau contenant les informations de l'utilisateur (il prend la valeur de la variable $ligne)
+                    foreach ($ligne as $column) {
+                        echo "<td>$column</td>";
+                    }
                     
+                    echo "<td> 
+                        <button name='modify' value='$ligne->id' formaction='Views/update.php'>Modifier</button> 
+                        <button name='delete' value='$ligne->id' formaction='Controllers/delete_ctrl.php'>Supprimer</button> 
+                    </td>";
+
+                    echo '</tr>';
                 }
-            }
             ?>
         </table>
     </form>
+
+
 
 </body>
 </html>
