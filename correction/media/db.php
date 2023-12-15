123bcd6da1d1fb594b562c5d59d234b1ca837927
theovbl <tvirbel@gmail.com>
2023-12-15 16:32:29 +0100

    application

--- correction/media/db.php
+++ correction/media/db.php
@@ -1,3 +1,3 @@
 <?php
-$bdd = new PDO ('mysql:host=localhost;dbname=cours;charset=utf8','Theo','Azerty123'); 
+$bdd = new PDO ('mysql:host=localhost;dbname=cours;charset=utf8','THEO','Azerty123'); 
 // Connexion à la base de donnée mysql avec ses identifiants
